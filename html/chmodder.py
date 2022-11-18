13f26aa39ebc31acf0188ea7cf58c14998a819cb
andmydignity <ahmetaslan139@outlook.com.tr>
2022-11-18 13:07:51 +0300

    no diff.

--- html/chmodder.py
+++ html/chmodder.py
@@ -5,6 +5,6 @@ while True:
     os.chdir(folder)
     for somefile in os.listdir('.'):
         os.system("sudo chmod 777 {}".format(somefile))
-        os.system("sudo chown ubuntu:ubuntu {}".format(somefile))
+        os.system("sudo chown ubuntu:users {}".format(somefile))
     time.sleep(15)
 
