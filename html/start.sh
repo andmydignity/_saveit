b68899f68f281159f826aa4c560605743097f4d8
andmydignity <ahmetaslan139@outlook.com.tr>
2022-11-18 18:47:09 +0300

    Added start script.

--- html/start.sh
+++ html/start.sh
@@ -1,1 +1,2 @@
+#TODO:Make #4 it NGINX compatible.
 sudo gunicorn -w 16 -b 0.0.0.0:443 --certfile=/etc/letsencrypt/live/saveit.gq-0001/fullchain.pem --keyfile=/etc/letsencrypt/live/saveit.gq-0001/privkey.pem main:site
