7259f88a8478cea1f91882f52384254bfdada467
Semih Aslan <110099203+andmydignity@users.noreply.github.com>
2022-11-18 18:48:56 +0300

    Update start.sh

--- html/start.sh
+++ html/start.sh
@@ -1,2 +1,2 @@
 #TODO:Make #4 it NGINX compatible.
-sudo gunicorn -w 16 -b 0.0.0.0:443 --certfile=/etc/letsencrypt/live/saveit.gq-0001/fullchain.pem --keyfile=/etc/letsencrypt/live/saveit.gq-0001/privkey.pem main:site
+sudo gunicorn -w 16 -b 0.0.0.0:443 --certfile=/etc/letsencrypt/live/DOMAINFOLDERHERE/fullchain.pem --keyfile=/etc/letsencrypt/live/DOMAINFOLDERHERE/privkey.pem main:site
